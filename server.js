af294c457189f970e5245ab2354898a0983bbab6
MUGESH SELVARAJ <mugeshs.22cse@ongu.edu>
2025-11-21 15:32:05 +0530

    Implemented auth routes

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import auth_router from './routes/authRoutes'
 
 const app = express()
 const port = 8080
@@ -6,8 +7,11 @@ const port = 8080
 app.use(express.json())
 app.use(express.urlencoded())
 
+
 app.get("/", (req, res) => {
     res.send("<h1>Api is Working!</h1>")
 })
+
+app.post("/auth", auth_router)
 
 app.listen(port, () => console.log(`Server is listening in port ${port}`))
