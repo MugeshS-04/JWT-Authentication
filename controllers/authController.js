3f9b25958275d7565a9fd5e175b91a83f9182f74
MUGESH SELVARAJ <mugeshs.22cse@ongu.edu>
2025-11-21 17:28:24 +0530

    Implemented the login logic with access and refresh token

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,68 @@
-export const register = (req, res) => {
+import { reg_val, login_val } from "../routes/validation.js"
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 
+export const UserMap = new Map()
+
+export const register = async (req, res) => {
+
+    let valid = reg_val.validate(req.body)
+
+    if(valid)
+    {
+        let hashpass = await bcrypt.hash(req.body.pass, 10)
+        
+        let obj = {
+            name : req.body.name,
+            age : req.body.age,
+            email : req.body.email,
+            pass : hashpass
+        }
+
+        UserMap.set(obj.email, obj)
+
+        return res.json({success : true, message : "Account created Successfully!"})
+    }
+    else
+    {
+        return res.json({success : false, message : "Invalid details"})
+    }
 }
 
-export const login = (req, res) => {
-    
+export const login = async (req, res) => {
+
+    const valid = login_val.validate(req.body)
+
+    if(valid.error)
+    {
+        res.json({success : false, message : valid.error.message})
+    }
+    else
+    {
+        if(UserMap.has(req.body.email))
+        {
+            const pass_cmpr = await bcrypt.compare(req.body.pass, UserMap.get(req.body.email).pass)
+
+            if(pass_cmpr)
+            {
+                let access_token = jwt.sign({key : req.body.email}, process.env.JWT_ACCESS_SECRET_KEY, {expiresIn : '1d'})
+                let refresh_token = jwt.sign({key : req.body.email}, process.env.JWT_REFRESH_SECRET_KEY, {expiresIn : '30d'})
+
+                res.cookie('refresh_token', refresh_token, {
+                    httpOnly : true,
+                    secure : true
+                })
+
+                res.json({success : true, message : "Login Successfull!", access_token : access_token})
+            }
+            else
+            {
+                res.json({success : false, message : "Password is Incorrect"})
+            }
+        }
+        else
+        {
+            res.json({success : false, message : "No Data Available"})
+        }
+    }
 }
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 import express from 'express'
-import auth_router from './routes/authRoutes'
+import dotenv from 'dotenv'
+import auth_router from './routes/authRoutes.js'
 
 const app = express()
 const port = 8080
 
+dotenv.config()
 app.use(express.json())
 app.use(express.urlencoded())
 
@@ -12,6 +14,6 @@ app.get("/", (req, res) => {
     res.send("<h1>Api is Working!</h1>")
 })
 
-app.post("/auth", auth_router)
+app.use("/auth/", auth_router)
 
 app.listen(port, () => console.log(`Server is listening in port ${port}`))
