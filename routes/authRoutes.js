627ca943b099d1c8424d79ad87bf51b8eaf10d10
MUGESH SELVARAJ <mugeshs.22cse@ongu.edu>
2025-11-25 12:03:19 +0530

    Fixed the bug, where validate middleware was not called before

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,11 +1,12 @@
 import express from 'express'
 import { register, login, refresh } from '../controllers/authController.js'
 import { jwt_verify_refresh } from '../helpers/token.js'
+import { register_validate, login_validate } from './validation.js'
 
 const auth_router = express.Router()
 
-auth_router.post("/register", register)
-auth_router.post("/login", login)
+auth_router.post("/register", register_validate, register)
+auth_router.post("/login", login_validate, login)
 auth_router.post("/refresh", jwt_verify_refresh, refresh)
 
 export default auth_router
